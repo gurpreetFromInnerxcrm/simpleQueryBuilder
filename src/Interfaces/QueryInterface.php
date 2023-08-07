f048441ef22ef02138995f5adf09b4ad9ccd67c8
robinkashyap <gurpreetkait.codes@gmail.com>
2023-08-07 09:28:17 -0800

    added new structure

--- src/Interfaces/QueryInterface.php
+++ src/Interfaces/QueryInterface.php
@@ -1,14 +1,30 @@
 <?php
 
-namespace Querybuilder\Query;
+namespace robinksp\querybuilder\Interfaces;
 
-
-interface QueryInterface
+interface QueryBuilderInterface
 {
-    public function select(...$columns): self;
-    public function from(string $table): self;
-    public function where($condition): self;
-    public function join($join): self;
-    // Add more methods for other parts of the query.
-    public function build(): string;
+    public function table(string $table);
+
+    public function select($columns);
+
+    public function join(string $table, string $firstColumn, string $operator, string $secondColumn, string $type = 'INNER');
+
+    public function groupBy($columns);
+
+    public function take($limit);
+
+    public function get();
+
+    public function count();
+
+    public function orderBy(string $column, string $direction = 'ASC');
+
+    public function delete();
+
+    public function insert(array $data):int ;
+
+    public function update(array $data):int;
+
+    public function setPage(int $page = 1, int $perPage = 10):self;
 }
--- src/Query.php
+++ src/Query.php
@@ -2,87 +2,205 @@
 
 namespace robinksp\querybuilder;
 
-use Exception;
+use Closure;
+use PDO;
+use robinksp\querybuilder\Components\DebugTrait;
 use robinksp\querybuilder\Connection\Mysql;
+use robinksp\querybuilder\Interfaces\QueryBuilderInterface;
+use robinksp\querybuilder\Components\WhereClausesTrait;
 
-
-class Query extends Mysql
+class Query extends Mysql implements QueryBuilderInterface
 {
-    protected $select = '*';
+    use WhereClausesTrait,DebugTrait;
+    private $table;
+    private $select = '*';
+    private $where = [];
+    private $bindings = [];
+    private $joins = [];
+    private $groupBy = [];
+    protected $orderBy = '';
+    protected $limit;
+    protected $offset;
 
-    protected $insert = [];
-    protected $table;
-    protected $where = [];
-    protected $joins = [];
+    protected int $page;
+    protected int $perPage;
 
-    public function select(...$columns): self
+    public function __construct()
     {
-        $this->select = implode(', ', $columns);
-        return $this;
+        parent::__construct();
     }
 
-    public function table(string $table): self
+    public function table(string $table)
     {
         $this->table = $table;
         return $this;
     }
 
-    public function where($condition): self
+    public function select($columns)
     {
-        $this->where[] = $condition;
+        $this->select = is_array($columns) ? implode(', ', $columns) : $columns;
         return $this;
     }
 
-    public function join($join): self
+    public function join(string $table, string $firstColumn, string $operator, string $secondColumn, string $type = 'INNER')
     {
-        $this->joins[] = $join;
+        $this->joins[] = "$type JOIN $table ON $firstColumn $operator $secondColumn";
         return $this;
     }
-
-    public function insert(array $data)
+    public function groupBy($columns)
     {
-        if (empty($this->table) || empty($data)) {
-            throw new Exception("Table and data must be set before building the insert query.");
-        }
-
-        $columns = implode(', ', array_keys($data));
-        $values = implode(', ', array_map(fn ($value) => $this->quoteValue($value), $data));
-        $query = "INSERT INTO {$this->table} ($columns) VALUES ($values)";
-
-        try {
-            $this->executeQuery($query);
-            return $this->pdo->lastInsertId();
-        } catch (\Throwable $th) {
-            throw $th;
-        }
-
+        $this->groupBy = is_array($columns) ? $columns : [$columns];
+        return $this;
     }
-    private function quoteValue($value)
+    public function take($limit)
     {
-        // Check if the value is a string, if so, escape and add quotes.
-        if (is_string($value)) {
-            return "'" . addslashes($value) . "'";
+        $this->limit = $limit;
+        return $this;
+    }
+    public function get()
+    {
+        if (!empty($this->page)) {
+            $page = !empty($this->page) ? $this->page : 1;
+            $perPage = !empty($this->perPage) ? $this->perPage : 10;
+            $this->offset = ($page - 1) * $perPage;
         }
 
-        // If it's not a string, just return the value as is.
-        return $value;
-    }
-    // Add more methods for other parts of the query.
-
-    public function build(): string
-    {
         $query = "SELECT {$this->select} FROM {$this->table}";
 
-        foreach ($this->joins as $join) {
-            $query .= " " . $join->build();
+        if (!empty($this->joins)) {
+            $query .= " " . implode(' ', $this->joins);
         }
 
         if (!empty($this->where)) {
-            $conditions = implode(' AND ', array_map(fn ($condition) => $condition->build(), $this->where));
-            $query .= " WHERE $conditions";
+            $query .= " WHERE " . $this->buildWhere();
         }
 
-        return $query;
+        if (!empty($this->groupBy)) {
+            $query .= " GROUP BY " . implode(', ', $this->groupBy);
+        }
+        if ($this->orderBy !== null && !empty($this->orderBy)) {
+            $query .= " ORDER BY " . $this->orderBy;
+        }
+
+        if ($this->limit !== null) {
+            $query .= " LIMIT " . $this->limit;
+        }
+
+        if ($this->offset !== null) {
+            $query .= " OFFSET " . $this->offset;
+        }
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($this->bindings);
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function count()
+    {
+        $query = "SELECT COUNT(*) as total FROM {$this->table}";
+
+        if (!empty($this->joins)) {
+            $query .= " " . implode(' ', $this->joins);
+        }
+
+        if (!empty($this->where)) {
+            $query .= " WHERE " . $this->buildWhere();
+        }
+
+        if (!empty($this->groupBy)) {
+            $query .= " GROUP BY " . implode(', ', $this->groupBy);
+        }
+
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($this->bindings);
+
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        return isset($result['total']) ? (int) $result['total'] : 0;
+    }
+
+    public function orderBy(string $column, string $direction = 'ASC')
+    {
+        // Validate the sorting direction to ensure it is either 'ASC' or 'DESC'
+        $direction = strtoupper($direction);
+        if ($direction !== 'ASC' && $direction !== 'DESC') {
+            throw new \InvalidArgumentException("Invalid sorting direction. Only 'ASC' or 'DESC' are allowed.");
+        }
+
+        $this->orderBy = "$column $direction";
+        return $this;
+    }
+
+    private function buildWhere()
+    {
+        $whereClauses = [];
+
+        foreach ($this->where as $condition) {
+            $column = $condition['column'];
+            $operator = $condition['operator'];
+            $value = $condition['value'];
+            $boolean = $condition['boolean'];
+
+            if ($value instanceof Closure) {
+                $builder = new self();
+                $value($builder);
+                $value = '(' . $builder->buildWhere() . ')';
+            } else {
+                $value = '?';
+            }
+
+            $whereClauses[] = "$column $operator $value $boolean";
+        }
+        return implode(' ', $whereClauses);
+    }
+
+    public function delete()
+    {
+        $query = "DELETE FROM {$this->table}";
+
+        if (!empty($this->where)) {
+            $query .= " WHERE " . $this->buildWhere();
+        }
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($this->bindings);
+
+        return $statement->rowCount();
+    }
+    public function setPage($page = 1, $perPage = 10):self
+    {
+        $this->page = $page;
+        $this->perPage = $perPage;
+        return $this;
+
+    }
+    public function insert(array $data) : int
+    {
+        $columns = implode(', ', array_keys($data));
+        $placeholders = implode(', ', array_fill(0, count($data), '?'));
+
+        $query = "INSERT INTO {$this->table} ($columns) VALUES ($placeholders)";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(array_values($data));
+
+        return $this->pdo->lastInsertId();
+    }
+
+    public function update(array $data): int
+    {
+        $setClauses = [];
+
+        foreach ($data as $column => $value) {
+            $setClauses[] = "$column = ?";
+            $this->bindings[] = $value;
+        }
+
+        $query = "UPDATE {$this->table} SET " . implode(', ', $setClauses);
+
+        if (!empty($this->where)) {
+            $query .= " WHERE " . $this->buildWhere();
+        }
+
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($this->bindings);
+
+        return $statement->rowCount();
     }
 
 }
